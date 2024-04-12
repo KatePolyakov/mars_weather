e470e2bf144915172d6ec8ca7a95cd1d9f03026c
Fernanda <pazocampo.fer@gmail.com>
2024-04-12 12:16:58 -0700

    Adding structure and styles for weather cards

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+
+
+
+  return (
+    <div className="app">
+      <div className="app-container">
+       <h1 className='app-container__title'>MARS InSight Mission</h1>
+       <h2 className='app-container__subtitle'> Latest Weather at Elysium Planitia</h2>
+        <div className='app-weather'>
+          <div className='app-weather__grid'>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+                <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+            <div className='app-weather__card'>
+              <div className='app-weather__content'>
+              <div className='app-weather__wrapper'>
+                  <p className='app-weather__sol'>SOL</p>
+                  <p className='app-weather__date'>Oct. 19 </p>
+                </div>
+                <p className='app-weather__high'>High: 39°F</p>
+                <p className='app-weather__low'>Low: -87°F</p>
+                <p className='app-weather__sunrise'>Sunrise: 05:29 am</p>
+                <p className='app-weather__sunset'>Sunset: 05:42 pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
